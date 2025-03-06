5c8228ea5208b2f66ead24a9219f8777543dede5
Ian <ian.njoroge2@student.moringaschool.com>
2025-03-06 18:06:30 +0300

    index.js

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
 
-// Write your code here
+    const num1 = 31;
+    const num2 = 2;
+    
+        const multiply=(num1 * num2);
+   
+//console.log(multiply());  
+
+
+
+    const random = Math.floor(Math.random() * 100) + 1;
+
+
+
+
+
+
+const num3 = 20;
+const num4 = 8;
+const mod = num3 % num4;
+console.log(mod);
+
+const max = Math.max(5, 10, 15, 20); 
+console.log(max);
